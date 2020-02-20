30b5373ebf33e78c5b26dc1c5e085f4c9321a6c8
Michele Murakami <mmurakam@ucsd.edu>
2020-02-20 00:08:07 -0800

    added analytics

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,13 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(request, response){
-  	response.render('index', projects);
+    //response.render('index', projects["viewAlt"]=false);
+    projects["viewAlt"] = false;
+    response.render('index', projects);
+};
+
+exports.viewAlt = function(request, response){
+  	//response.render('index', projects["viewAlt"]=true);
+    projects["viewAlt"] = true;
+    response.render('index', projects);
 };
